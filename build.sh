f7892517a8895597b2763bdb6e6d343c654d0068
Kai Li <ykzihel@gmail.com>
2017-02-14 23:30:15 -0500

    chore: rename release files

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ while [[ $# -gt 0 ]]; do
   case $1 in
   --es6)
     echo "building es6"
-    ${rollup} -i index.js -o ./dist/turkopticon.es6.user.js -f iife --banner "$banner"
+    ${rollup} -i index.js -o ./dist/turkoptiscript.es6.user.js -f iife --banner "$banner"
     shift ;;
   --es5)
     echo "building es5"
     # piping to cat here to get rid of extraneous new lines added during transpiling
-    ${rollup} -c --banner "$banner" | cat -s > ./dist/turkopticon.es5.user.js
+    ${rollup} -c --banner "$banner" | cat -s > ./dist/turkoptiscript.es5.user.js
     shift ;;
   -a|--all)
     $0 --es6 --es5; exit 0;;
